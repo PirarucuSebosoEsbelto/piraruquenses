d02ca31c7660b0bf70f065b149c37aef590466d8
PirarucuSebosoEsbelto <133917215+PirarucuSebosoEsbelto@users.noreply.github.com>
2024-09-25 08:48:46 -0300

    a

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ console.log(areaQuadrado(6));
 
 
 
-let nome= parseint(prompt("valores?"));
-document write(valores);
-
-
+let num1= parseInt(prompt("Insira o primeiro número"))
+let num2= parseInt(prompt("Insira o segundo número"))
+let num3= parseInt(prompt("Insira o terceiro número"))
+document.write(media(num1,num2,num3))
 
 let media= function(num1,num2,num3){
 return (num1+num2+num3)/3;
