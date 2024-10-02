26defe16be6e045465bd4ecfe26e67773e615018
PirarucuSebosoEsbelto <133917215+PirarucuSebosoEsbelto@users.noreply.github.com>
2024-10-02 07:38:45 -0300

    A

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-function soma(num1,num2){
-return num1+num2;
+// function soma(num1,num2){
+// return num1+num2;
 
-}
-console.log(soma(5,2));
-
-
-
-function areaQuadrado(lado){
-
-    return lado**2;
-}
-console.log(areaQuadrado(6));
+// }
+// console.log(soma(5,2));
 
 
 
-let num1= parseInt(prompt("Insira o primeiro número"))
-let num2= parseInt(prompt("Insira o segundo número"))
-let num3= parseInt(prompt("Insira o terceiro número"))
-document.write(media(num1,num2,num3))
+// function areaQuadrado(lado){
 
-let media= function(num1,num2,num3){
-return (num1+num2+num3)/3;
-
-}
-console.log(media(3,2,1));
+//     return lado**2;
+// }
+// console.log(areaQuadrado(6));
 
 
+
+// let num1= parseInt(prompt("Insira o primeiro número"))
+// let num2= parseInt(prompt("Insira o segundo número"))
+// let num3= parseInt(prompt("Insira o terceiro número"))
+// document.write(media(num1,num2,num3))
+
+// let media= function(num1,num2,num3){
+// return (num1+num2+num3)/3;
+
+// }
+// console.log(media(3,2,1));
+
+let estados=["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO"];
